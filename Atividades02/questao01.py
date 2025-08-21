16e65031f86283429395fcf256a196375fa3663b
Nilton Fernandes <niltonfernandes86@gmail.com>
2025-08-21 09:39:34 -0300

    atividade02 questão01

---
+++ Atividades02/questao01.py
@@ -0,0 +1,18 @@
+"""1- Conversor de Moeda
+Crie um programa que converte um valor em reais para dólares e euros. 
+Use os seguintes dados:
+
+* Valor em reais: R$ 100.00
+* Taxa do dólar: R$ 5.20
+* Taxa do euro: R$ 6.15
+O programa deve calcular e exibir os valores convertidos, arredondando 
+para duas casas decimais."""
+
+reais = 100
+dolar = 5.20
+euro = 6.15
+
+conv_real_dolar = reais / dolar
+cone_real_euro = reais / euro
+
+print(f"Valores. \nReais: {reais:.2f}. \nDolar: {conv_real_dolar:.2f}. \nEuro: {cone_real_euro:.2f}.")
