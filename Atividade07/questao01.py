90f3f48e2cc32124a474ef6db0e904b0cdf26871
Nilton Fernandes <niltonfernandes86@gmail.com>
2025-08-29 11:41:42 -0300

    questao01

---
+++ Atividade07/questao01.py
@@ -0,0 +1,27 @@
+"""1 - 
+Leia um arquivo que contém dados de log de treinamento de modelos de Machine Learning.
+ Calcule a média e o desvio padrão do tempo de execução constantes. 
+"""
+import pandas as pd
+
+def processar_logs_treinamento(nome_arquivo):
+    try:
+        leitor = pd.read_csv(nome_arquivo)
+        media_tempo = leitor['tempo_execucao'].mean()
+        desvio_padrao = leitor['tempo_execucao'].std()
+        return f"""
+                Média do tempo de execução: {media_tempo}
+                Desvio padrao do tempo de execução: {desvio_padrao:.2f}
+                """
+    except FileNotFoundError:
+        return "Arquivo não encontrado."
+    
+
+nome_arquivo = input("Digite o nome do arquivo csv: ")
+
+print(processar_logs_treinamento(nome_arquivo))  
+
+            
+
+
+
