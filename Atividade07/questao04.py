e1b1a8b1dadb4ebece411c06cd21bc21351fcf85
Nilton Fernandes <niltonfernandes86@gmail.com>
2025-08-29 11:43:16 -0300

    questao04

---
+++ Atividade07/questao04.py
@@ -0,0 +1,33 @@
+"""4 - 
+
+Crie um script em Python que leia e escreva dados em um arquivo JSON. 
+O arquivo JSON deve conter informações de uma pessoa, com campos nome, idade e cidade."""
+
+import json
+
+def ler_json(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo_json:
+            dados_json = json.load(arquivo_json)
+            print(dados_json)
+    except FileNotFoundError:
+        return(f"Arquivo não encontrado")
+    
+
+def escrever_json(nome_arquivo, dados):
+    try:
+        with open(nome_arquivo, 'w', encoding='utf-8') as arquivo_json:
+            json.dump(dados, arquivo_json, ensure_ascii = False, indent = 4)
+        print(f"Dados salvos em: {nome_arquivo}")
+    except Exception as e:
+        return(f"Erro ao escrever no arquivo: {e}")
+    
+dados = {
+    "nome": "João",
+    "idade": 20,
+    "cidade": "São Paulo"
+}
+
+nome_arquivo = input("Digite o nome do arquivo: ")
+escrever_json(nome_arquivo, dados)
+ler_json(nome_arquivo)
